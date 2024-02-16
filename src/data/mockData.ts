0058f37dbd1e14df93e257fc839a4d7dbcb161c9
Alejandro Chang <alejandrochang10@gmail.com>
2024-02-15 17:25:13 -0800

    feat(km): holy spirit devo pt 1

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,7 +1,48 @@
 import BuildingIntimacyGod from "../assets/BuildingIntimacyGod.webp";
+import TheHolySpirit from "../assets/theholyspirit.webp";
 
 export const mockData: BlogListProps = {
   posts: [
+    {
+      id: "the-holy-spirit",
+      title: "The Holy Spirit",
+      content:
+        "Who is the Holy Spirit? Why did the father send the Holy Spirit? How do we receive the Holy Spirit? How does the Holy Spirit Help Us in general and understand the bible? What does it mean to be filled with the Spirit? What is the result of being filled with the Spirit? These and more of some of the questions we try to uncomver in this devotional.",
+      date: "2024-01-25",
+      url: "https://www.bible.com/reading-plans/19085-the-holy-spirit",
+      coverImg: TheHolySpirit,
+      series: "We Want More Holy Spirit",
+      scriptures: [
+        { verse: "Acts 1:8", day: 1 },
+        { verse: "John 14:15-17", day: 1 },
+        { verse: "John 14:25-26", day: 1 },
+        { verse: "John 16:12-15", day: 1 },
+        { verse: "Romans 8:9-11", day: 2 },
+        { verse: "John 14:15-17", day: 2 },
+        { verse: "John 14:25-26", day: 2 },
+        { verse: "John 16:12-15", day: 2 },
+        { verse: "Isaiah 29:13", day: 2 },
+        { verse: "Mark 10:17-22", day: 2 },
+        { verse: "Acts 2:38", day: 3 },
+        { verse: "1 Corinthians 6:11", day: 3 },
+        { verse: "Galatians 3:2-3", day: 3 },
+        { verse: "1 Corinthisans 2:12", day: 4 },
+        { verse: "John 14:16-17", day: 4 },
+        { verse: "John 16:13-14", day: 4 },
+        { verse: "Galatians 5:22-23", day: 4 },
+        { verse: "1 Corinthians 2:14-16", day: 5 },
+        { verse: "Acts 4:31", day: 6 },
+        { verse: "1 Corinthians 12:7-11", day: 6 },
+      ],
+      notes: [
+        "Day 1: Who is the Holy Spirit? - The Spirit of Jesus that gives us acess to the loving, saving, healing and powerful presence of God! The Bible tells us that the Holy Spirit is: God (third person of the trinity), the Spirit of Jesus whom Jesus sent when he ascended into heaven, always with us, gives us salvation and sanctification, gives us power to conquer sin and fear, gives us strength to be Jesus' witnesses in the world.",
+        "Day 2: Why did the Father and Son send the Holy Spirit? - To give life to his people and dwell with them forever. The Holy Spirit is life, joy and love. Jesus sent his Spirit to make us effective witnesses in the world and holy and righteous in the eyes of the Lord. ",
+        "Day 3: How do we receive the Holy Spirit? - Through Gods grace through faith in Jesus Christ. The Holy Spirit can't be tamed or controlled. Hes soverign, unpredictable and trustworthy. As you have faith in the Lord Jesus and what he has done, the free gift of the Holy Spirit becomes evident so that you can become more like Jesus.",
+        "Day 4: How does the Holy Spirit help us? - The Holy Spirit leads us into all truth. The Spirit makes the Word of God come alive to us. The Holy Spirit gives us life! Renews our mind, and transforms our hearts.",
+        "Day 5: How does the Holy Spirit help us understand the Bible? - The Holy Spirit leads us to Jesus. He cleanses our sins and makes us holy. When we open our hearts, our bibles, the Holy Spirit will be present! The Holy Spirit bears witness to Jesus.",
+        "Day 5: What does it mea to be filled with the Spirit? - The Spirit of God helps us be in alignment with Gods purpose for our life. The Spiritual Gifts are always for a purpose and higher calling. The Spirit fills us with Gods power and helps us to grow in our relationship with Jesus. The Spirit also fills us with Gods love, to move through us to others.",
+      ],
+    },
     {
       id: "building-intimacy-with-god",
       title: "Building Intimacy with God",
@@ -26,12 +67,13 @@ export const mockData: BlogListProps = {
         { verse: "Hebews 10:23-25", day: 5 },
         { verse: "1 Thessalonians 5:10-11", day: 5 },
       ],
-      notes: ["Day 1: Is Intimacy With God Possible? - What does intimacy with God practically mean? Since the Spirit of revelation allows us to know him better, how van we get closer to Him? It is definitely possible, but requires our participation. The Lord says to seek him with all your heart and soul and then you will find him.",
-      "Day 2: Barriers to Intimacy with God - Any Barriers that you're currently facing that prevent you from reaching new levels of intimacy?",
-      "Day 3: Worshipping God with our Hears - So how can we get closer to God? Any ways you can think of that have worked for you or haven't? Some ways we can get closer to his is through worship and surrender. Intimacy means developing a pattern or rythmn of life that is centered on God. When we fall in love with Jesus obedience will natural flow from you.",
-      "Day 4: Making Intimacy with God a lifetime Priority - Do you think intimacy with God is a priority? How can our actions demonstrate this? Intimacy with God will become a regular priority when you believe it is absolutely critical for you to function.",
-      "Day 5: What is Your Next Step? - What is your next step in building intimacy with God? What is one thing you can do today to take that step? What are some very practical steps that can lead us to his presence?",
-    ],
+      notes: [
+        "Day 1: Is Intimacy With God Possible? - What does intimacy with God practically mean? Since the Spirit of revelation allows us to know him better, how van we get closer to Him? It is definitely possible, but requires our participation. The Lord says to seek him with all your heart and soul and then you will find him.",
+        "Day 2: Barriers to Intimacy with God - Any Barriers that you're currently facing that prevent you from reaching new levels of intimacy?",
+        "Day 3: Worshipping God with our Hears - So how can we get closer to God? Any ways you can think of that have worked for you or haven't? Some ways we can get closer to his is through worship and surrender. Intimacy means developing a pattern or rythmn of life that is centered on God. When we fall in love with Jesus obedience will natural flow from you.",
+        "Day 4: Making Intimacy with God a lifetime Priority - Do you think intimacy with God is a priority? How can our actions demonstrate this? Intimacy with God will become a regular priority when you believe it is absolutely critical for you to function.",
+        "Day 5: What is Your Next Step? - What is your next step in building intimacy with God? What is one thing you can do today to take that step? What are some very practical steps that can lead us to his presence?",
+      ],
     },
   ],
 };
